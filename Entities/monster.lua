9afeefbf56f9254111c1c09f54ff85b6cbdc72ad
xiongchiamiov <xiong.chiamiov@gmail.com>
2014-01-25 10:57:14 -0800

    Draw Monsters from their feet
    Fixes #9.
    
    http://cl.ly/image/0r2l433d063z

--- Entities/monster.lua
+++ Entities/monster.lua
@@ -10,7 +10,7 @@ local Monster = Class{
    end;
 
    draw = function(self, time)
-      love.graphics.draw(self.image, self.position.x, self.position.y)
+      love.graphics.draw(self.image, self.position.x - Tile.SIZE / 2, self.position.y - Tile.SIZE, 0, 1, 1, Tile.SIZE / 2, Tile.SIZE)
    end;
 }
 
