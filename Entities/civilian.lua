a3015f9c681341e4267086f8c5d3f9ec2aa6cf08
xiongchiamiov <xiong.chiamiov@gmail.com>
2014-01-25 22:25:54 -0800

    Switch inheritance of Monster and Civilian

--- Entities/civilian.lua
+++ Entities/civilian.lua
@@ -1,16 +1,34 @@
 image = love.graphics.newImage('Assets/monster1.png')
 
 local Civilian = Class{
-   __includes = {Monster};
+   __includes = {Character};
    init = function(self, x, y)
-		Monster.init(self, x, y)
+      self:setPositionFromTopLeft(x, y)
+      self.image = image
+      self:addBoundingBox()
+      self.health = 3
       self.damage = 0
    end;
    
    update = function(self, dt)
-		Monster.update(self, dt)
-		self.image = image
+      self:checkMissiles()
    end;
+   
+   checkMissiles = function(self)
+      for i, missile in ipairs(missiles) do
+         if self.boundingBox:collidesWith(missile.boundingBox) then
+            self.health = self.health - missile.damage
+            table.remove(missiles, i)
+         end
+      end
+   end;
+   
+   effect = function(self)
+      return -self.damage
+   end;
+
+   VISION_DISTANCE = 3 * Tile.SIZE;
+   MOVE_DISTANCE = 2 * Tile.SIZE;
 }
 
 return Civilian
--- Entities/monster.lua
+++ Entities/monster.lua
@@ -1,6 +1,3 @@
-Player = require 'Entities.player'
-Tile = require 'Entities.tile'
-
 local imgs = {
    love.graphics.newImage('Assets/monster1.png'),
    love.graphics.newImage('Assets/monster2.png'),
@@ -16,17 +13,14 @@ local imgs = {
 }
 
 local Monster = Class{
-   __includes = {Character};
+   __includes = {Civilian};
    init = function(self, x, y)
-      self:setPositionFromTopLeft(x, y)
-      self.image = imgs[11]
-      self:addBoundingBox()
-      self.health = 3
+		Civilian.init(self, x, y)
       self.damage = 1
    end;
    
    update = function(self, dt)
-      self:checkMissiles()
+		Civilian.update(self, dt)
       
       self.image = imgs[math.ceil(redPillEffectiveness)]
 
@@ -36,22 +30,6 @@ local Monster = Class{
          self:move(vecToPlayer.x, vecToPlayer.y)
       end
    end;
-   
-   checkMissiles = function(self)
-      for i, missile in ipairs(missiles) do
-         if self.boundingBox:collidesWith(missile.boundingBox) then
-            self.health = self.health - missile.damage
-            table.remove(missiles, i)
-         end
-      end
-   end;
-   
-   effect = function(self)
-      return -self.damage
-   end;
-
-   VISION_DISTANCE = 3 * Tile.SIZE;
-   MOVE_DISTANCE = 2 * Tile.SIZE;
 }
 
 return Monster
